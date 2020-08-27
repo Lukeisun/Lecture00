5a6f29f8a9de51845d05c6ed5d171501cb9b982a
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2020-08-27 17:47:53 -0400

    First version :)

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("bop");
+        for(String a : args){
+            System.out.println(a);
+        }
+    }
+}
